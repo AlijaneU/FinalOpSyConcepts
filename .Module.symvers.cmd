5503e064f1465bcc5a3af98ff0c78aaea466351b
AlijaneU <126324212+AlijaneU@users.noreply.github.com>
2023-02-25 16:42:42 +0600

    Delete .Module.symvers.cmd

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/kernel_modules/Module.symvers := sed 's/ko$$/o/' /root/kernel_modules/modules.order | scripts/mod/modpost -m -a  -o /root/kernel_modules/Module.symvers -e -i Module.symvers   -T -
